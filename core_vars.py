ebd361ebd9152b78484eb29d3b84df4fffdbd028
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2025-03-11 13:24:46 -0400

    Create core_vars.py

---
+++ core_vars.py
@@ -0,0 +1,12 @@
+# Screen dimensions
+WIDTH, HEIGHT = 800, 600
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# Darkening effect higher value = less darkening
+DARKENING_FACTOR = 60
+# Rendering distance
+RENDER_DISTANCE_FAR = DARKENING_FACTOR
+RENDER_DISTANCE_BEHIND = 0
+RENDER_DISTANCE_RIGHT = 50
+RENDER_DISTANCE_LEFT = -RENDER_DISTANCE_RIGHT
