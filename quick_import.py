6a52e3591cfefe00ba66a8fe9e5d0f9cee75a396
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2025-03-26 13:40:26 -0400

    Update quick_import.py

--- quick_import.py
+++ quick_import.py
@@ -1,4 +1,4 @@
-import total_modles
+import quick_add_modles
 from main import os 
 from main import np
 
@@ -6,19 +6,20 @@ QUICK_IMPORT_DATA = {}
 
 
 def what_file_type():
-    for imported_file in os.listdir(total_modles.__path__[0]):
-        if imported_file.endswith(".stl"):
+    for imported_file in os.listdir(quick_add_modles.__path__[0]):
+        print(imported_file)
+        if str(imported_file).endswith(".stl"):
             import stl
             # Load the STL file
-            mesh_data = stl.mesh.Mesh.from_file(imported_file)
+            mesh_data = stl.mesh.Mesh.from_file(quick_add_modles.__path__[0] + "/" + imported_file)
             
             # Extract vertices and faces
             vertices = mesh_data.vectors.reshape((-1, 3))
             faces = np.arange(len(vertices)).reshape((-1, 3))
             
             general_data_final(vertices, faces, imported_file) 
-        elif imported_file.endswith(".obj"):
-            with open(imported_file, 'r') as file:
+        elif str(imported_file).endswith(".obj"):
+            with open(quick_add_modles.__path__[0] + "/" + imported_file, 'r') as file:
                 for line in file:
                     if line.startswith('v '):
                         parts = line.strip().split()
@@ -29,11 +30,13 @@ def what_file_type():
                         face = tuple(int(idx.split('/')[0]) - 1 for idx in parts[1:5])
                         faces.append((face, (255, 255, 255)))  # Default color white
             general_data_final(vertices, faces, imported_file)
+        elif str(imported_file).endswith(".py"):
+            None
         else:
             print("The file type is not supported")
 
 def general_data_final(vertices, faces, imported_file):
-    imported_file = {
+    QUICK_IMPORT_DATA[imported_file] = {
         "vertices": vertices.tolist(),
         "pivot": (0, 0, 0),
         "edges": [],
@@ -41,5 +44,3 @@ def general_data_final(vertices, faces, imported_file):
             (face.tolist(), (255, 0, 0)) for face in faces
         ]
     }
-    QUICK_IMPORT_DATA.append(imported_file)
-    
